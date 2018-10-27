dbf9fd2b5d1ac41e73c4a851d613399d4731d9b9
simple-npm-package <nikitanagorni@mail.ru>
2018-10-27 12:42:02 +0300

    add change

--- 06/main.js
+++ 06/main.js
@@ -1,7 +1,5 @@
 var array1 = [123, "string", true, {value: 321}, null],
     array2 = ["first", "second", "last"],
-    result = [];
-Array.prototype.push.apply(result, array1);
-Array.prototype.push.apply(result, array2);
+    result = array1.concat(array2);
 
 console.log("result: ", result);
