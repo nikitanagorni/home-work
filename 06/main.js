6e86916b8ab9a25e0c6a87d04563f8de0c967ce2
simple-npm-package <nikitanagorni@mail.ru>
2018-11-15 23:38:04 +0300

    added change

--- 06/main.js
+++ 06/main.js
@@ -1,10 +1,15 @@
 var array1 = [123, "string", true, {value: 321}, null],
     array2 = ["first", "second", "last"],
     result = [];
-while (result < array1.length) {
-    result++
-    result += array1;
-    result += array2;
+for (var unite = 0; unite < array1.length; unite++) {
+  	result[unite] = array1[unite];
 }
+var i = 0
+do {
+  	result[unite] = array2[i];
+  	unite++;
+  	i++;
+} 
+while (i != 3);
 console.log("result: ", result);
 
